6d328570e10a1a2ab7f8ecf50b8cc1fa6ed42f2d
Thalita85 <thaliolivergomes@gmail.com>
2024-08-26 18:16:14 +0000

    Executamos desafios

--- Desafio.java
+++ Desafio.java
@@ -1,4 +1,21 @@
-import 
+package livraria;
+
+public class livro{
+
+string titulo;
+int numero de Paginas;
+double precos;
+
+void cadastrer(){
+    
+    System.out.println("Cadastrando livro");
+}
+    double calfret(String cep){
+        double frete = 0;
+        System.out.println("Calculando o frete para o cep " + cep );
+        returne frete;
+    }
+}
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Orient_Objt
+
